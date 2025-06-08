77a592defba876bc1edfc2474f95d542b8d896d8
b5050d <bdyer5280@gmail.com>
2025-06-07 21:22:11 -0600

    Alright fixed the last filepath issue, should work now. Also should have fixed the CI pipeline to only run the web app tests for the web app job

--- services/stl_generation/tests/test_process_byte_streams.py
+++ services/stl_generation/tests/test_process_byte_streams.py
@@ -6,7 +6,8 @@ from stl_generation.modules.process_byte_streams import cookie_cutter
 
 def test_generate_stl():
     currdir = os.path.dirname(__file__)
-    with open(currdir + "\\test_hawaii_white.bin", "rb") as f:
+    filepath = os.path.join(currdir, "test_hawaii_white.bin")
+    with open(filepath, "rb") as f:
         data = f.read()
 
     response = cookie_cutter(data)
